cadfd6dc6cd5e909b829a8424f2b651e0ea29886
aminamaludin <aminamaludin001@gmail.com>
2018-05-15 11:40:19 +0700

    first commit

---
+++ app/src/main/java/com/example/windows10/kampret/Bantul.java
@@ -0,0 +1,13 @@
+package com.example.windows10.kampret;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bantul extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bantul);
+    }
+}
